6a0081669c9695854da7c227df7ae37c700ac616
Rahul chakraborty <rahulchakraborty337@gmail.com>
2024-06-03 11:04:48 +0530

    Navbar modifications - Mobile menu - Navbar for bigger screens - - searchbar - - icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,37 @@
+import Link from "next/link";
 import React from "react";
+import Menu from "./Menu";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+            {/* Mobile */}
+            <div className="h-full flex item-center justify-between md:hidden">
+                <Link href="/">
+                    <div className="text-2xl tracking-wide">Rahulc0dy</div>
+                </Link>
+                <Menu />
+            </div>
+            {/* Bigger Screens */}
+            <div className="hidden md:flex items-center justify-between gep-8 h-full">
+                {/* Left */}
+                <div className="w-1/3">
+                    <Link href="/">
+                        <Image src="/logo.png" alt="" width={24} height={24} />
+                        <div className="text-2xl tracking-wide">Rahulc0dy</div>
+                    </Link>
+                </div>
+                {/* Right */}
+                <div className="w-2/3 flex items-center justify-between gap-8 ">
+                    <SearchBar />
+                    <NavIcons />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
